37f5f5bd401477fe2e191bf86a24a1f4824fc3c1
Dan Tao <daniel.tao@gmail.com>
2012-02-23 21:13:17 -0800

    simplified Sum category

--- Objective-LinqTests/NSArraySumSpec.m
+++ Objective-LinqTests/NSArraySumSpec.m
@@ -8,11 +8,7 @@ describe(@"NSArray+Select", ^{
     NSArray *integers = [NSArray arrayWithObjects:theValue(1), theValue(2), theValue(3), nil];
 
     it(@"calculates the sum of elements in the array", ^{
-        int sum = [integers sumInts:^(int x, int y) {
-            return x + y;
-        }];
-
-        [[theValue(sum) should] equal:theValue(6)];
+        [[theValue([integers sumInts]) should] equal:theValue(6)];
     });
 });
 
